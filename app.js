fdd80b885ad721d686354169db9426039acbe0a5
Muhammad Sufyan <hafiz.sufyansadiq@gmail.com>
2023-12-18 22:47:14 +0500

    Update app.js

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 import { initializeApp }  from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
 import { getFirestore , doc  ,collection, addDoc , getDocs , updateDoc   } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
-// const firebaseConfig = {
-//   apiKey: "AIzaSyDnL1QO4hHQbdr_-iqjVRbNgGOUQb8lSjw",
-//   authDomain: "e-commerece-d1da6.firebaseapp.com",
-//   projectId: "e-commerece-d1da6",
-//   storageBucket: "e-commerece-d1da6.appspot.com",
-//   messagingSenderId: "441885728322",
-//   appId: "1:441885728322:web:a8c5cd02a60335fc0697db"
-// };
+const firebaseConfig = {
+  apiKey: "AIzaSyDnL1QO4hHQbdr_-iqjVRbNgGOUQb8lSjw",
+  authDomain: "e-commerece-d1da6.firebaseapp.com",
+  projectId: "e-commerece-d1da6",
+  storageBucket: "e-commerece-d1da6.appspot.com",
+  messagingSenderId: "441885728322",
+  appId: "1:441885728322:web:a8c5cd02a60335fc0697db"
+};
 
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
