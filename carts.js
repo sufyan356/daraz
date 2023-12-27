c57a07154def0ec4c7c05c68965ab1f821eac128
Muhammad Sufyan <hafiz.sufyansadiq@gmail.com>
2023-12-28 02:18:55 +0500

    changes  mostly file

--- carts.js
+++ carts.js
@@ -11,165 +11,195 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 
-window.DeleteFunction = async (productId) => {    // window.DeleteFunction  ( function run successfully )
-    console.log("delete function run")
+window.DeleteFunction = async (productId , price) => {    // window.DeleteFunction  ( function run successfully )
+    console.log("delete function run");
     try {
+        let total;const totalAmountSnapshot = await getDocs(collection(db, "totalAmount"));
+        totalAmountSnapshot.forEach((ele) => {
+            total = (ele.data().total - price).toFixed(3)
+            console.log(total)
+            
+        })
+        const totals = doc(db, "totalAmount", totalAmountSnapshot.docs[0].id);
+        await updateDoc(totals, {
+            total
+        });
+        console.log(total)
+
         await deleteDoc(doc(db, "Quantity", productId));
         console.log("Document successfully deleted!");
+
+        
         window.location.reload()
     } catch (error) {
         console.error("Error deleting document: ", error);
     }
 };
 
+
+
 let total = 0;
 let docRef; // Declare docRef outside the function
-
-window.ItemsSelected = async (event, price, quantity) => {
-    let subTotalID = document.querySelector("#subTotalID");
-    let totalID = document.querySelector("#totalID");
+let totalAmountRef; // Declare docRef outside the function
+let isChecked;
+window.ItemsSelected = async (event, price, quantity, productID) => {
     const querySnapshot = await getDocs(collection(db, "SelectedItems"));
-    querySnapshot.forEach((doc) => {
-        console.log(doc.data());
-    });
-
-    if (event.target.checked) {
-        total += +price * quantity;
-        docRef = await addDoc(collection(db, "SelectedItems"), {
-            total
-        });
-        console.log("Document written with ID: ", docRef.id);
-    } else {
-        if (docRef) { // Check if docRef is defined
-            total -= +price * quantity;
-            const washingtonRef = doc(db, "SelectedItems", docRef.id);
-            await updateDoc(washingtonRef, {
-                total
+    if (querySnapshot.empty) {
+        if (event.target.checked) {
+            total += +price * quantity;
+            // console.log(total)
+            totalAmountRef = await addDoc(collection(db, "totalAmount"), { total});
+            docRef = await addDoc(collection(db, "SelectedItems"), {
+                total,
+                productID,
+                price,
+                quantity,
+                isChecked: true
             });
-        } else {
-            console.warn("docRef is not defined. Unable to update total.");
+        }
+    } 
+
+
+    else {
+        const existingItem = querySnapshot.docs.find(doc => doc.data().productID === productID);
+        const totalAmountSnapshot = await getDocs(collection(db, "totalAmount"));
+        let a = 0;
+       
+
+        if (existingItem) {
+            console.log("DB KHALI NHIHAI.")
+            if (event.target.checked) {
+                total += +price * quantity;
+                // console.log(total)
+                const totals = doc(db, "totalAmount", totalAmountSnapshot.docs[0].id);
+                await updateDoc(totals, {
+                    total
+                });
+
+                totalAmountSnapshot.forEach((ele) => {
+                    a+=ele.data().total
+                    console.log(a)
+                })
+
+                const washingtonRef = doc(db, "SelectedItems", existingItem.id);
+                await updateDoc(washingtonRef, {
+                    total,
+                    productID,
+                    price,
+                    quantity,
+                    isChecked: true
+                });
+            } else {
+                total -= +price * quantity;
+                // console.log(total)
+                const totals = doc(db, "totalAmount", totalAmountSnapshot.docs[0].id);
+                await updateDoc(totals, {
+                    total
+                });
+                totalAmountSnapshot.forEach((ele) => {
+                    a+=ele.data().total
+                    console.log(a)
+                })
+
+                const washingtonRef = doc(db, "SelectedItems", existingItem.id);
+                await updateDoc(washingtonRef, {
+                    total,
+                    productID,
+                    price,
+                    quantity,
+                    isChecked: false
+                });
+            }
+        } 
+        else {
+            console.log("DB KHALI HAI.")
+            if (event.target.checked) {
+                total += +price * quantity;
+               
+                const totals = doc(db, "totalAmount", totalAmountSnapshot.docs[0].id);
+                await updateDoc(totals, {
+                    total
+                });
+                totalAmountSnapshot.forEach((ele) => {
+                    a+=ele.data().total
+                    console.log(a)
+                     console.log(total)
+                })
+                docRef = await addDoc(collection(db, "SelectedItems"), {
+                    total,
+                    productID,
+                    price,
+                    quantity,
+                    isChecked: true
+                });
+            }
         }
     }
 
-    subTotalID.innerHTML = total.toFixed(3);
-    totalID.innerHTML = total.toFixed(3);
-    console.log("totalPrice: " + total.toFixed(3));
+    updateTotalUI();
+};
+
+const updateTotalUI = async () => {
+    let subTotalID = document.querySelector("#subTotalID");
+    let totalID = document.querySelector("#totalID");
+    let totalAmount;
+    subTotalID.innerHTML = total;
+    totalID.innerHTML = total;
+};
+
+let setID;
+const checkedFunction = async () => {
+    const SelectedItemsQuerySnapshot = await getDocs(collection(db, "SelectedItems"));
+    const checkboxes = document.querySelectorAll("#cardMini input[type='checkbox']");
+
+    checkboxes.forEach((checkbox) => {
+        const checkboxId = checkbox.id;
+        const isChecked = SelectedItemsQuerySnapshot.docs.some((element) => element.data().productID == checkboxId && element.data().isChecked);
+        console.log(isChecked)
+        checkbox.checked = isChecked;
+    });
+
+    for(const element of SelectedItemsQuerySnapshot.docs){
+        setID = element.id
+        console.log(element.id + ": " + element.data().isChecked + "\n")
+    }
 };
 
 
 
+let cardMinis = document.querySelector("#cardMini");
 const cardsData = async () => {
-   
-    let cardMinis = document.querySelector("#cardMini");
     const productsQuerySnapshot = await getDocs(collection(db, "Quantity"));
-        for (const productDoc of productsQuerySnapshot.docs) {
-            // console.log(productDoc.data())
-            cardMinis.innerHTML +=`
-            <div class="card "id = "shopCards">
-                <input type="checkbox" id="selectCart" onclick = "ItemsSelected(event , ' ${productDoc.data().price}' , ' ${productDoc.data().quantity}')" >
-                <img src="${productDoc.data().img}" class="card-img-top" alt="..." id = "cartsImage">
+
+    for (const productDoc of productsQuerySnapshot.docs) {
+        cardMinis.innerHTML += `
+            <div class="card "id="shopCards">
+                <input type="checkbox" id="${productDoc.data().productId}" onclick="ItemsSelected(event, ${productDoc.data().price}, ${productDoc.data().quantity}, '${productDoc.data().productId}')">
+                <img src="${productDoc.data().img}" class="card-img-top" alt="..." id="cartsImage">
                 <div class="card-body cardsBody ">
-                <h5 class="card-title price">Price: ${productDoc.data().price}</h5>
-                <p class="card-text">Quantity: ${productDoc.data().quantity}</p>
-                <i class="fa-solid fa-trash trash" onclick="DeleteFunction('${productDoc.id}')"></i>
+                    <h5 class="card-title price">Price: ${productDoc.data().price}</h5>
+                    <p class="card-text">Quantity: ${productDoc.data().quantity}</p>
+                    <i class="fa-solid fa-trash trash" onclick=" DeleteFunction('${productDoc.id}' , ${productDoc.data().price})"></i>
                 </div>
             </div>
-            `
-        }
-  };
-  const loader = () => {
-    setTimeout(() => {
-        cardsData()
-    },3000)
-  }
+        `;
+    }
+    
+};
+
+const loader = async () => {
+   
+
+    await cardsData();
+        const selectedItemsCollection = collection(db, "totalAmount");
+        const selectedItemsQuerySnapshot = await getDocs(selectedItemsCollection);
+    
+        selectedItemsQuerySnapshot.forEach((itemDoc) => {
+            total += itemDoc.data().total;
+        });
+
+        await checkedFunction();
+        await updateTotalUI();
+};
 
 window.addEventListener("DOMContentLoaded", loader);
-
-///////////////////////////////////////////////////////////
-// let total = 0;
-// let docRef; // Declare docRef outside the function
-
-// window.ItemsSelected = async (event, price, quantity) => {
-//     let subTotalID = document.querySelector("#subTotalID");
-//     let totalID = document.querySelector("#totalID");
-
-//     if (event.target.checked) {
-//         total += +price * quantity;
-//         docRef = await addDoc(collection(db, "SelectedItems"), {
-//             total,
-//             isChecked: true
-//         });
-//         console.log("Document written with ID: ", docRef.id);
-//     } else {
-//         if (docRef) {
-//             total -= +price * quantity;
-//             const washingtonRef = doc(db, "SelectedItems", docRef.id);
-//             await updateDoc(washingtonRef, {
-//                 total,
-//                 isChecked: false
-//             });
-//         } else {
-//             console.warn("docRef is not defined. Unable to update total.");
-//         }
-//     }
-
-//     updateTotalUI();
-// };
-
-// const updateTotalUI = () => {
-//     let subTotalID = document.querySelector("#subTotalID");
-//     let totalID = document.querySelector("#totalID");
-//     subTotalID.innerHTML = total.toFixed(3);
-//     totalID.innerHTML = total.toFixed(3);
-//     console.log("totalPrice: " + total.toFixed(3));
-// };
-
-// const getCheckedState = async (productId) => {
-//     const itemDoc = await getDoc(doc(db, "SelectedItems", productId));
-//     return itemDoc.exists() && itemDoc.data().isChecked;
-// };
-
-// const cardsData = async () => {
-//     let cardMinis = document.querySelector("#cardMini");
-//     const productsQuerySnapshot = await getDocs(collection(db, "Quantity"));
-
-//     for (const productDoc of productsQuerySnapshot.docs) {
-//         const isChecked = await getCheckedState(productDoc.id);
-//         cardMinis.innerHTML += `
-//             <div class="card "id="shopCards">
-//                 <input type="checkbox" id="${productDoc.id}" onclick="ItemsSelected(event, ${productDoc.data().price}, ${productDoc.data().quantity})" ${isChecked ? 'checked' : ''}>
-//                 <img src="${productDoc.data().img}" class="card-img-top" alt="..." id="cartsImage">
-//                 <div class="card-body cardsBody ">
-//                     <h5 class="card-title price">Price: ${productDoc.data().price}</h5>
-//                     <p class="card-text">Quantity: ${productDoc.data().quantity}</p>
-//                     <i class="fa-solid fa-trash trash" onclick="DeleteFunction('${productDoc.id}')"></i>
-//                 </div>
-//             </div>
-//         `;
-//     }
-// };
-
-// const loader = () => {
-//     setTimeout(async () => {
-//         await cardsData();
-//         const selectedItemsCollection = collection(db, "SelectedItems");
-//         const selectedItemsQuerySnapshot = await getDocs(selectedItemsCollection);
-
-//         selectedItemsQuerySnapshot.forEach((itemDoc) => {
-//             total += itemDoc.data().total;
-//             const checkbox = document.getElementById(itemDoc.id);
-//             if (checkbox) {
-//                 checkbox.checked = true;
-//             }
-//         });
-
-//         updateTotalUI();
-//     }, 3000);
-// };
-
-// window.addEventListener("DOMContentLoaded", loader);
-
-
-
-///////////////////////////////////////////////////////////
-  
